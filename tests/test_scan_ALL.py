53c73a77201d8039091ae920f4c8600362ed1d33
Wenjun Wu <wenjun.wu@ga.gov.au>
2019-07-09 14:22:02 +1000

    added assert to verify result in the .all file.

--- tests/test_scan_ALL.py
+++ tests/test_scan_ALL.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import time
 from hyo2.mate.lib.scan_ALL import ScanALL
+from hyo2.mate.lib import scan
 
 TEST_FILE1 = "0200_MBES_EM122_20150203_010431_Supporter_GA4430.all"
 TEST_FILE = "0243_P007_MBES_EM122_20150207_044356_Supporter_GA4430.all"
@@ -20,44 +21,50 @@ class TestMateScanALL(unittest.TestCase):
         return self.test._time_str(time.time())
 
     def test_get_datagram_info(self):
-        self.test.get_datagram_info('D')
+        D = self.test.get_datagram_info('D')
+        self.assertEqual(D, None)
+        Y = self.test.get_datagram_info('Y')
+        self.assertEqual(Y['byteCount'], 81386)
 
     def test_get_size_n_pings(self):
         self.test.get_size_n_pings(2)
 
     def test_pings(self):
-        self.test.get_total_pings('f')
-        self.test.get_missed_pings('f')
-        self.test.get_total_pings('-')
-        self.test.get_missed_pings('-')
-        self.test.get_total_pings()
-        self.test.get_missed_pings()
-        self.test.is_missing_pings_tolerable()
-        self.test.has_minimum_pings()
+        self.assertEqual(self.test.get_total_pings('N'), 11)
+        self.assertEqual(self.test.get_missed_pings('N'), 0)
+        self.assertEqual(self.test.get_total_pings('-'), 0)
+        self.assertEqual(self.test.get_missed_pings('-'), 0)
+        self.assertEqual(self.test.get_total_pings(), 31)
+        self.assertEqual(self.test.get_missed_pings(), 0)
+        self.assertTrue(self.test.is_missing_pings_tolerable())
+        self.assertTrue(self.test.has_minimum_pings())
 
     def test_is_size_matched(self):
-        self.test.is_size_matched()
+        self.assertTrue(self.test.is_size_matched())
 
     def test_is_filename_changed(self):
-        self.test.is_filename_changed()
+        self.assertTrue(self.test.is_filename_changed())
 
     def test_is_date_match(self):
-        self.test.is_date_match()
+        self.assertTrue(self.test.is_date_match())
 
     def test_bathymetry_availability(self):
-        self.test.bathymetry_availability()
+        self.assertEqual(self.test.bathymetry_availability(),
+                         scan.A_PARTIAL)
 
     def test_backscatter_availability(self):
-        self.test.backscatter_availability()
+        self.assertEqual(self.test.backscatter_availability(),
+                         scan.A_PARTIAL)
 
     def test_ray_tracing_availability(self):
-        self.test.ray_tracing_availability()
+        self.assertFalse(self.test.ray_tracing_availability())
 
     def test_ellipsoid_height_availability(self):
-        self.test.ellipsoid_height_availability()
+        self.assertTrue(self.test.ellipsoid_height_availability())
 
     def test_PU_status(self):
-        self.test.PU_status()
+        self.assertEqual(self.test.PU_status(),
+                         scan.A_PASS)
 
 
 def suite():
